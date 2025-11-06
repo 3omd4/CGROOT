567958c5e354e8efd5028971c66b88967508f97d
3omd4 <mohamedemadeldin38@gmail.com>
2025-11-06 18:53:05 +0200

    added the declarations in spu_kernels.h

--- src/math/cpu_kernels.h
+++ src/math/cpu_kernels.h
@@ -1,3 +1,41 @@
+#ifndef CPU_KERNELS_H
+#define CPU_KERNELS_H
+namespace cgroot {
+namespace math {
+
+template <typename T>
+void cpu_add(T* out, const T* in1, const T* in2, unsigned long long size);
+
+template <typename T>
+void cpu_mul(T* out, const T* in1, const T* in2, unsigned long long size);
+
+template <typename T>
+void cpu_matmul(T* out, const T* A, const T* B, unsigned long long M, unsigned long long N, unsigned long long K);
+
+template <typename T>
+void cpu_relu(T* out, const T* in, unsigned long long size);
+
+template <typename T>
+void cpu_sigmoid(T* out, const T* in, unsigned long long size);
+
+template <typename T>
+void cpu_relu_backward(T* grad_in, const T* grad_out, const T* input, unsigned long long size);
+
+template <typename T>
+void cpu_sigmoid_backward(T* grad_in, const T* grad_out, const T* input, unsigned long long size);
+
+} // namespace math
+} // namespace cgrootwhat 
+
+#endif
+
+
+
+
+
+
+
+
 /*
 Purpose: Header file declaring all low-level CPU math functions.
 To-Do:
@@ -13,4 +51,5 @@ void cpu_sigmoid(T* out, const T* in, size_t size);
 ...etc. for all ops.
 Backward Kernels:
 void cpu_relu_backward(T* grad_in, const T* grad_out, const T* input, size_t size);
-void cpu_sigmoid_backward(T* grad_in, const T* grad_out, const T* input, size_t size);*/
+void cpu_sigmoid_backward(T* grad_in, const T* grad_out, const T* input, size_t size);
+*/
