311e9e1074501e375990988ff50c9aa265659bbe
3omd4 <mohamedemadeldin38@gmail.com>
2025-11-14 15:40:18 +0200

    deleted files from old structure once we need it we will add it again

--- src/autograd/graph.h
+++
@@ -1,5 +0,0 @@
-/*Purpose: Declares the main backward function that performs the graph traversal.
-
-To-Do:
-Declare void backward(Tensor<T>& start_tensor). This function will be called by Tensor::backward().
-*/
--- src/nn/sigmoid.h
+++
@@ -1,7 +0,0 @@
-/*Purpose: A Sigmoid activation layer.
-
-To-Do:
-
-class Sigmoid : public Module. (No parameters).
-
-sigmoid.cpp (forward): return input.sigmoid();.*/
